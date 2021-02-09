55801897ae10b7875e2b5222dec04939d6661b1c
Aliaksandr Torkhau <torkhau@gmail.com>
2021-02-09 14:47:56 +0300

    solution vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,58 @@
 const CustomError = require("../extensions/custom-error");
-
+const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(machine = true) {
+    this.machine = machine;
+  }
+  
+  encrypt(message, key) {
+    if (!(message || key)) throw 'THROWN';
+    message = message.toUpperCase();
+    const messageLaters = message.replace(/[^A-Z]/g, '');
+    while (key.length < messageLaters.length) {
+      key += key.repeat(1);
+    }
+    key = key.substr(0, messageLaters.length).toUpperCase();
+    let encript = '';
+    let indexMessage = 0;
+    let indexKey = 0;
+    do {
+      const m = alphabet.indexOf(message[indexMessage]);
+      const k = alphabet.indexOf(key[indexKey]);
+      if (m >= 0 && k >= 0) {
+        encript += alphabet[(m + k) % 26];
+        indexKey += 1;
+      } else {
+        encript += message[indexMessage];
+      }
+      indexMessage += 1;
+    } while (indexMessage !== message.length);
+    return this.machine ? encript : encript.split('').reverse().join('');
+  }
+
+  decrypt(message, key) {
+    if (!(message || key)) throw 'THROWN';
+    message = message.toUpperCase();
+    const messageLaters = message.replace(/[^A-Z]/g, '');
+    while (key.length < messageLaters.length) {
+      key += key.repeat(1);
+    }
+    key = key.substr(0, messageLaters.length).toUpperCase();
+    let decrypt = '';
+    let indexMessage = 0;
+    let indexKey = 0;
+    do {
+      const m = alphabet.indexOf(message[indexMessage]);
+      const k = alphabet.indexOf(key[indexKey]);
+      if (m >= 0 && k >= 0) {
+        decrypt += alphabet[(m + 26 - k) % 26];
+        indexKey += 1;
+      } else {
+        decrypt += message[indexMessage];
+      }
+      indexMessage += 1;
+    } while (indexMessage !== message.length);
+    return this.machine ? decrypt : decrypt.split('').reverse().join('');
   }
 }
 
